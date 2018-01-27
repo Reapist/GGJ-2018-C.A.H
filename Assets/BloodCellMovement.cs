2345c8f615c03e2e10dc9f115d8fea1ae0ce955b
ph34rl3ssL34d3r <ph34rl3ssL34d3r@users.noreply.github.com>
2018-01-27 20:55:33 +0900

    Animation

---
+++ Assets/BloodCellMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodCellMovement : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+		float translation;
+		var xPlus = 0;
+		var xMinus = 0;
+		var yPlus = 0;
+		var yMinus = 0;
+
+		if ((Random.value < .5f) && (yMinus < 5))
+		{
+			translation = Time.deltaTime * -.1f;
+			yMinus++;
+			if (yPlus > 0)
+				yPlus--;
+		}
+		else
+		{
+			translation = Time.deltaTime * .1f;
+			yPlus++;
+			if (yMinus > 0)
+				yMinus--;
+		}
+		transform.Translate(0, translation, 0);
+
+		if ((Random.value < .5f) && (xMinus < 5))
+		{
+			translation = Time.deltaTime * -.1f;
+			xMinus++;
+			if (xPlus > 0)
+				xPlus--;
+		}
+		else
+		{
+			translation = Time.deltaTime * .1f;
+			xPlus++;
+			if (xMinus > 0)
+				xMinus--;
+		}
+		transform.Translate(translation, 0, 0);
+	}
+}
