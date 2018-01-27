7211826fb0d8c315996070476b7559e4193e9545
J <J@J.J>
2018-01-27 21:47:42 +0900

    WIP

--- Assets/SmallRecolor.cs
+++ Assets/SmallRecolor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SmallRecolor : MonoBehaviour {
 
@@ -21,11 +22,18 @@ public class SmallRecolor : MonoBehaviour {
 		{
 			collider.tag = "ControlledA";
 			collider.GetComponent<Renderer>().material.color = Color.red;
+			collider.GetComponent<Renderer>().materials[0].color = Color.red;
+			collider.GetComponent<Renderer>().materials[1].color = Color.red;
+			GameObject.Find("Text").GetComponent<Text>().text = collider.name;
 		}
 		else if (gameObject.tag == "PlayerB" && (collider.tag == "ControlledA" || collider.tag == "Neutral"))
 		{
 			collider.tag = "ControlledB";
 			collider.GetComponent<Renderer>().material.color = Color.blue;
+			foreach (Material m in collider.GetComponent<Renderer>().materials)
+			{
+				m.color = Color.blue;
+			}
 		}
 	}
 }
